f4ebf023bf6418b871292a4d6fc94f1c293858b0
Prashant <plm1535172@gmail.com>
2024-10-02 22:29:59 +0530

    Server and Weather, both files are modified

--- weather.py
+++ weather.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 def get_current_weather(city="Kansas City"):
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={
-        os.getenv("API_KEY")}&q={city}&units=imperial'
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
 
     weather_data = requests.get(request_url).json()
 
@@ -16,9 +16,14 @@ def get_current_weather(city="Kansas City"):
 
 
 if __name__ == "__main__":
-    print("\n*** Get Current Weather Conditions *** \n")
+    print('\n*** Get Current Weather Conditions ***\n')
 
-    city = input("\nPlease enter a city name:")
+    city = input("\nPlease enter a city name: ")
+
+    # Check for empty strings or string with only spaces
+    # This step is not required here
+    # if not bool(city.strip()):
+    #     city = "Kansas City"
 
     weather_data = get_current_weather(city)
 
